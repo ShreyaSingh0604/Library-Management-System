813d2ce481e5905a2fda072224595a1d235852b1
ShreyaSingh0604 <111371314+ShreyaSingh0604@users.noreply.github.com>
2022-10-17 11:04:28 +0530

    Update Library Management System.cpp

--- Library Management System.cpp
+++ Library Management System.cpp
@@ -1,8 +1,9 @@
 #include<iostream> 
-#include<conio.h>
+#include<conio.h> //We have used getch() command which will help us to hold the output screen by taking dummy input from the user
 #include<string>
-#include<stdlib.h>
 using namespace std;
+//counter for Book array
+int counter=0;
 
 class Book {
 	private:
@@ -21,30 +22,25 @@ class Book {
 		string getEdition(){return edition;}
 		string getPublication(){return publication;}
 };
-//counter for Book array
-int counter=0;
 
 
 //initializing functions with counter as parameter
-void addBook(int counter);
-void deleteBook(int counter);
+void addBook(int& counter);
+void deleteBook(int& counter);
 void editBook(int counter);
 void searchBook(int counter);
 void viewAllBooks(int counter);
 void quit();
 
 
-
 //function for incrementing counter
-void increment(int a){
-	a++;
-	counter=a;
+inline void increment(int &counter1){
+	counter1++;
 }
 
 //function for decrementing counter
-void decrement(int a){
-	a--;
-	counter=a;
+inline void decrement(int &counter1){
+	counter1--;
 }
 
 //Book class array initialization
@@ -52,51 +48,51 @@ Book books[10];
 
 //main function
 int main(){
-string choice;
-//Main Menu
-
-system("CLS");
-cout<<"LIBRARY MANAGEMENT SYSTEM\n\n";
-cout<<"[1]ADD BOOK\n";	
-cout<<"[2]DELETE BOOK\n";	
-cout<<"[3]EDIT BOOK\n";	
-cout<<"[4]SEARCH BOOK\n";	
-cout<<"[5]VIEW ALL BOOKS\n";	
-cout<<"[6]QUIT\n\n";
-
-cout<<"ENTER CHOICE: ";
-getline(cin,choice);
-system("CLS");
-
-if(choice=="1"){
-	addBook(counter); //function call
-}		
-else if(choice=="2"){	
-	deleteBook(counter); //function call
-}
-else if(choice=="3"){
-	editBook(counter); //function call	
-}
-else if(choice=="4"){
-	searchBook(counter); //function call	
-}
-else if(choice=="5"){
-	viewAllBooks(counter); //function call	
-}
-else if(choice=="6"){
-	quit();	 //function call
-}
-else{
-	main();  //function call to self(main)
-}
+	string choice;
+	//Main Menu
 	
-_getch();
-return 0;
+	system("CLS");
+	cout<<"LIBRARY MANAGEMENT SYSTEM\n\n";
+	cout<<"[1]ADD BOOK\n";	
+	cout<<"[2]DELETE BOOK\n";	
+	cout<<"[3]EDIT BOOK\n";	
+	cout<<"[4]SEARCH BOOK\n";	
+	cout<<"[5]VIEW ALL BOOKS\n";	
+	cout<<"[6]QUIT\n\n";
+	
+	cout<<"ENTER CHOICE: ";
+	getline(cin,choice);
+	system("CLS");
+	
+	if(choice=="1"){
+		addBook(counter); //function call
+	}		
+	else if(choice=="2"){	
+		deleteBook(counter); //function call
+	}
+	else if(choice=="3"){
+		editBook(counter); //function call	
+	}
+	else if(choice=="4"){
+		searchBook(counter); //function call	
+	}
+	else if(choice=="5"){
+		viewAllBooks(counter); //function call	
+	}
+	else if(choice=="6"){
+		quit();	 //function call
+	}
+	else{
+		main();  //function call to self(main)
+	}
+		
+	_getch();
+	return 0;
 }
 
 
 //addBook function
-void addBook(int counter){
+void addBook(int& counter){
 	string isbn,title,author,edition,publication;
 	cout<<"ADD BOOK\n\n";
 	if(counter<10){
@@ -128,7 +124,7 @@ void addBook(int counter){
 }
 
 //deleteBook function
-void deleteBook(int counter){
+void deleteBook(int& counter){
 	string isbn;
 	int choice;
 	cout<<"DELETE BOOK\n\n";
@@ -147,20 +143,10 @@ void deleteBook(int counter){
 			cout<<"Do you want to delete?\n[1] for Yes\n[2] for No\n\nEnter Choice: ";
 			cin>>choice;
 			if(choice==1){
-				books[i].setIsbn(""); //setting the value to none
-				books[i].setTitle("");
-				books[i].setAuthor("");
-				books[i].setEdition("");
-				books[i].setPublication("");
 				for(int a=i;a<counter;a++){
 					//adjusting the values after deletion of data eg. data from book[4] copied to book[3]
 					books[a] = books[a+1];
 				}
-				books[9].setIsbn(""); //adjustment if the library is full(10 books)
-				books[9].setTitle("");
-				books[9].setAuthor("");
-				books[9].setEdition("");
-				books[9].setPublication("");
 				decrement(counter); //calling function to decrement counter
 				cout<<"\nBOOK SUCCESSFULLY DELETED!\n\nPress any key to continue . . .";
 				getch();
@@ -269,12 +255,14 @@ void viewAllBooks(int counter){
 	//iterating all the values on the library using book array
 	cout<<"VIEW ALL BOOKS\n\n";
 	for(int i=0;i<counter;i++){
-		cout<<"BOOK DETAILS\n\n";
+		cout<<"\nBOOK Number: "<<(i+1)<<endl;
+		cout<<"--------------------------------\n";
 		cout<<"ISBN: "<<books[i].getIsbn()<<endl;
 		cout<<"TITLE: "<<books[i].getTitle()<<endl;
 		cout<<"AUTHOR: "<<books[i].getAuthor()<<endl;
 		cout<<"EDITION: "<<books[i].getEdition()<<endl;
-		cout<<"PUBLICATION: "<<books[i].getPublication()<<endl<<endl;
+		cout<<"PUBLICATION: "<<books[i].getPublication()<<endl;
+		cout<<"--------------------------------\n\n";
 	}
 	cout<<"Press any key to continue . . .";
 	getch();
